b7dafe9d8a5dc1c95984f2b8c1afe6edf937188d
Rafael Freitas <rafa.oliveirafreitas@hotmail.com>
2019-10-22 14:00:10 -0300

    adjusting layout

--- project-contribuitor-profile-rh/ContributorProfileRh/CustomCell.swift
+++ project-contribuitor-profile-rh/ContributorProfileRh/CustomCell.swift
@@ -10,33 +10,10 @@ import Foundation
 import UIKit
 
 class CustomCell: UITableViewCell {
+    var mainImage: UIImage?
     var title: String?
     var subtitle: String?
-    var mainImage: UIImage?
     
-    var titleView: UITextView = {
-        var titleView = UITextView()
-        titleView.translatesAutoresizingMaskIntoConstraints = false
-        titleView.isScrollEnabled = false
-        titleView.isEditable = false
-        titleView.font = UIFont(name: "HelveticaNeue-UltraLight", size: 12)
-        titleView.textColor = .link
-        
-        return titleView
-    }()
-    
-    
-    var subtitleView: UITextView = {
-        var subTitleView = UITextView()
-        subTitleView.translatesAutoresizingMaskIntoConstraints = false
-        subTitleView.isScrollEnabled = false
-        subTitleView.isEditable = false
-        subTitleView.font = UIFont.boldSystemFont(ofSize: 15)
-        subTitleView.textColor = .link
-        
-        return subTitleView
-    }()
-
     var mainImageView: UIImageView = {
         var mainImageView = UIImageView()
         mainImageView.translatesAutoresizingMaskIntoConstraints = false
@@ -44,35 +21,74 @@ class CustomCell: UITableViewCell {
         return mainImageView
     }()
     
+    var titleView: UILabel = {
+        var titleView = UILabel()
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.font = UIFont(name: "HelveticaNeue-UltraLight", size: 12)
+        titleView.textColor = .link
+        
+        return titleView
+    }()
+    
+    var lineView: UIView = {
+        let lineView = UIView()
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        lineView.backgroundColor = UIColor(white: 0.8, alpha: 0.8)
+        
+        return lineView
+    }()
+    
+    var subtitleView: UILabel = {
+        var subTitleView = UILabel()
+        subTitleView.translatesAutoresizingMaskIntoConstraints = false
+        subTitleView.font = UIFont.boldSystemFont(ofSize: 16)
+        subTitleView.textColor = .link
+        subTitleView.numberOfLines = 0
+        
+        return subTitleView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
+        setupView()
+    }
+    
+    func setupView() {
         self.addSubview(mainImageView)
         self.addSubview(titleView)
         self.addSubview(subtitleView)
+        self.addSubview(lineView)
+        
+        dataSubviews()
+        
         
         let constraints = [
-            mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 30),
-            mainImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
-            mainImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -20),
-            mainImageView.widthAnchor.constraint(equalTo: self.heightAnchor, constant: -30),
-        
+            mainImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
+            mainImageView.widthAnchor.constraint(equalToConstant: 40),
+            mainImageView.heightAnchor.constraint(equalToConstant: 40),
+            mainImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 18),
+            
+            titleView.topAnchor.constraint(equalTo: self.topAnchor, constant: 15),
             titleView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: 18),
             titleView.rightAnchor.constraint(equalTo: self.rightAnchor),
-            titleView.topAnchor.constraint(equalTo: self.topAnchor),
-            titleView.bottomAnchor.constraint(equalTo: self.mainImageView.bottomAnchor, constant: -15),
             
+            subtitleView.topAnchor.constraint(equalTo: self.titleView.bottomAnchor, constant: 5),
             subtitleView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: 18),
             subtitleView.rightAnchor.constraint(equalTo: self.rightAnchor),
-            subtitleView.topAnchor.constraint(equalTo: self.titleView.bottomAnchor, constant: -11),
-            subtitleView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+            
+            lineView.topAnchor.constraint(equalTo: self.subtitleView.bottomAnchor, constant: 15),
+            lineView.leftAnchor.constraint(equalTo: self.mainImageView.rightAnchor, constant: 18),
+            lineView.rightAnchor.constraint(equalTo: self.rightAnchor),
+            lineView.heightAnchor.constraint(equalToConstant: 1),
+            lineView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ]
         
         NSLayoutConstraint.activate(constraints)
     }
     
-    override func layoutSubviews() {
-        super.layoutSubviews()
+    func dataSubviews() {
+
         
         if let title = title {
             titleView.text = title
--- project-contribuitor-profile-rh/ContributorProfileRh/ViewController.swift
+++ project-contribuitor-profile-rh/ContributorProfileRh/ViewController.swift
@@ -14,17 +14,67 @@ struct CellData {
     let subtitle: String?
 }
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController{
     
     let cellId = "cellId"
     var data = [CellData]()
+    
+    lazy var tableView: UITableView = {
+        var tableView = UITableView()
+        
+        tableView.alwaysBounceVertical = true
+        tableView.alwaysBounceHorizontal = false
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = UITableViewCell.SeparatorStyle.none
+        tableView.separatorStyle = .none
+        tableView.backgroundColor = UIColor.white
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return tableView
+    }()
+    
+    lazy var btnAvaliarColaborador: UIButton = {
+        var btnAvaliarColaborador = UIButton()
+        
+        let yourColor : UIColor = .link
+        
+        btnAvaliarColaborador.layer.cornerRadius = 5
+        btnAvaliarColaborador.layer.borderWidth = 1
+        btnAvaliarColaborador.layer.borderColor = yourColor.cgColor
+        btnAvaliarColaborador.setTitleColor(.link, for: .normal)
+        btnAvaliarColaborador.setTitle("AVALIAR COLABORADOES", for: .normal)
+        btnAvaliarColaborador.translatesAutoresizingMaskIntoConstraints = false
+        
+        return btnAvaliarColaborador
+    }()
+    
+    var lineView: UIView = {
+        let lineView = UIView()
+        lineView.translatesAutoresizingMaskIntoConstraints = false
+        lineView.backgroundColor = UIColor(white: 0.8, alpha: 0.8)
+        
+        return lineView
+    }()
+    
+    var avaliacoesView : UILabel = {
+        var avaliacoesView = UILabel()
+        avaliacoesView.translatesAutoresizingMaskIntoConstraints = false
+        avaliacoesView.textColor = .link
+        avaliacoesView.numberOfLines = 0
+        avaliacoesView.text = "Média de 4,0 em 12 avaliações"
+        
+        return avaliacoesView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Perfil Colaborador"
         navigationController?.navigationBar.prefersLargeTitles = true
-       
+               
         data = [
             CellData.init(image: UIImage(named: "Colaborador"), title:"Colaborador", subtitle: "Rafael de Oliveira Freitas"),
             CellData.init(image: UIImage(named: "Departamento"), title:"Departamento", subtitle: "Inovação"),
@@ -32,27 +82,55 @@ class ViewController: UITableViewController {
             CellData.init(image: UIImage(named: "Funcao"), title:"Função", subtitle: "Software Developer")
         ]
         
+        setupView()
         tableView.register(CustomCell.self, forCellReuseIdentifier: cellId)
-        
         tableView.separatorStyle = .none
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 70
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! CustomCell
+    func setupView(){
+        view.addSubview(avaliacoesView)
+        view.addSubview(btnAvaliarColaborador)
+        view.addSubview(lineView)
+        view.addSubview(tableView)
         
-        cell.mainImage = data[indexPath.row].image
-        cell.title = data[indexPath.row].title
-        cell.subtitle = data[indexPath.row].subtitle?.uppercased()
+        avaliacoesView.topAnchor.constraint(equalTo: view.topAnchor, constant: 320).isActive = true
+        avaliacoesView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        btnAvaliarColaborador.topAnchor.constraint(equalTo: avaliacoesView.topAnchor, constant: 30).isActive = true
+        btnAvaliarColaborador.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        btnAvaliarColaborador.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 70).isActive = true
+        btnAvaliarColaborador.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -70).isActive = true
+        btnAvaliarColaborador.heightAnchor.constraint(equalToConstant: 45).isActive = true
+        
+        lineView.topAnchor.constraint(equalTo: self.btnAvaliarColaborador.bottomAnchor, constant: 24).isActive = true
+        lineView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        lineView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        lineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: self.lineView.topAnchor, constant: 8).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
     }
 }
 
+
+extension ViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+          return UITableView.automaticDimension
+      }
+      
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          let cell = CustomCell()
+          cell.mainImage = data[indexPath.row].image
+          cell.title = data[indexPath.row].title
+          cell.subtitle = data[indexPath.row].subtitle?.uppercased()
+          cell.setupView()
+          
+          return cell
+    }
+      
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return data.count
+    }
+}
